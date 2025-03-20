bfe0dae47cbdd8af1b3cb8e0f1f1da16fc6d680b
git--yasith02 <yasithuvanpriya@gmail.com>
2025-03-20 12:06:31 +0530

    Update constants.dart

--- BMI/lib/constants.dart
+++ BMI/lib/constants.dart
@@ -1,2 +1,7 @@
 import 'package:flutter/material.dart';
 const kTextColor =  Color(0xFFFF8888);
+const kInputLabelColor =TextStyle(
+                      color: kTextColor,
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                     );
